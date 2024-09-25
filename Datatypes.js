ae8b2faa4dc2933c110d41ff48b37b2a1d47b3f9
anurag123333 <159469213+anurag123333@users.noreply.github.com>
2024-09-25 06:09:08 +0000

    datatpes

---
+++ Datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3)  // we are using nodejs, not browser
+
+
+// let name = "anurag"
+// let age = 22
+// let isLoggedIn= false 
+
+//number => 2 to power 53
+
+//bigint
+//string => ""
+
+//boolean => true or false
+//null => stand alone value  //let state = null
+//undefined => value not assigned
+//symbol => find uniqueness
+
+
+//object =>
+
+console.log(typeof null);
+console.log(typeof undefined );
+
+
+
+
+
+
+
+
+
---
+++ datatype.js
@@ -0,0 +1,20 @@
+const accountId = 145533
+let accountEmail = "anuragkumar7483@gmail.com"
+var accountPassword = 12234
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 65878   cannot change the value of constant
+accountpassword = 34234
+accountCity = "Bettiah"
+
+
+// console.log(accountId
+// )
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+// why not use var ?
+
+//   because of issue in block scope and functional scope
