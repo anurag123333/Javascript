b688713ac5a2add1e41203bb3626290741613892
anurag123333 <159469213+anurag123333@users.noreply.github.com>
2024-09-25 06:29:28 +0000

    conversion

---
+++ conversion.js
@@ -0,0 +1,23 @@
+let age = true
+
+console.log(typeof age);
+console.log(typeof(age));
+
+let  valueInNumber = Number(age)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+let isLoggedIn ="Anurag"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let somenumbers = 33
+
+let stringNumber = String(somenumbers)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
